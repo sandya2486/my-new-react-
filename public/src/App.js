8d4db1ec3400ef9d14c02fb9bb0bb1d15f92b806
sandya2486 <asndya2486@gmail.com>
2022-07-06 10:40:20 +0530

    first commit

---
+++ public/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './Header/index'
+import Menu from './Menu/Menu';
+import {Footer} from './Footer'
+function App() {
+  return (
+    <div className="App"> 
+     <Header />
+     <Menu />
+     <Footer />
+     </div>
+  );
+}
+
+export default App;
